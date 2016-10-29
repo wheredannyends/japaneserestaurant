d583ccddb9c179b294314609d6ff6e0d05f93428
Danny <Danny@Dannys-MacBook-Pro.local>
2016-10-28 20:58:02 -0700

    Added more to locations, made small changes to other pages to implement JS

--- js/js.js
+++ js/js.js
@@ -1,3 +1,23 @@
+///// SMOOTH SCROLLING /////
+
+$(function () {
+  $('a[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - 100
+        }, 600);
+        return false;
+      }
+    }
+  });
+});
+
+
+///// HEADER /////
+
 $(window).on("scroll", function () {
   if ($(window).scrollTop() > 50) {
     $("header").addClass("sticky");
