1b722754bd36a36613ec7a02212f5955349c043e
Danny <Danny@dannys-mbp.wifi.bcit.ca>
2016-11-07 12:57:55 -0800

    Removed unnecessary JS

--- js/js.js
+++ js/js.js
@@ -25,23 +25,3 @@ $(window).on("scroll", function () {
     $("header").removeClass("sticky");
   }
 });
-
-
-///// HERO BUTTON /////
-
-$(function() {  
-  $('.btn-6')
-    .on('mouseenter', function(e) {
-			var parentOffset = $(this).offset(),
-      		relX = e.pageX - parentOffset.left,
-      		relY = e.pageY - parentOffset.top;
-			$(this).find('span').css({top:relY, left:relX})
-    })
-    .on('mouseout', function(e) {
-			var parentOffset = $(this).offset(),
-      		relX = e.pageX - parentOffset.left,
-      		relY = e.pageY - parentOffset.top;
-    	$(this).find('span').css({top:relY, left:relX})
-    });
-  $('[href=#]').click(function(){return false});
-});
